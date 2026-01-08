90920477da9c84709082926e43ea980dc09d81c3
jyccheun <jasoncheung0909@gmail.com>
2026-01-07 19:06:23 -0500

    video

--- src/sections/video/index.jsx
+++ src/sections/video/index.jsx
@@ -1,13 +1,13 @@
 import './.css'
 
+import vid from '../../assets/boston-temp-cut-BF7BR693.mp4'
+
 function Video() {
   return (
     <div className="overflow-hidden flex flex-col items-center pb-[25vh] bg-cool">
-      {/* <div className="video_container">
-        <video autoplay="" loop="">
-          <source src="/paul-landing/assets/boston-temp-cut-BF7BR693.mp4" type="video/mp4">
-        </video>
-      </div> */}
+      <div className="video_container">
+        <video src={vid} type="video/mp4" autoPlay muted />
+      </div>
     </div>
   )
 }
