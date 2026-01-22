9ebd529d062abb12a766dcca4cc030a4206bed03
jyccheun <jasoncheung0909@gmail.com>
2026-01-22 16:32:17 -0500

    test cloudfront s3 video

--- src/sections/video/index.jsx
+++ src/sections/video/index.jsx
@@ -7,7 +7,7 @@ function Video() {
   return (
     <div className="overflow-hidden flex flex-col items-center pb-[25vh] bg-cool">
       <div className="video_container">
-        <GumletPlayer
+        {/* <GumletPlayer
           videoID="6960b9b9525cbb355622b6c4"
           className="video_container"
           autoplay
@@ -16,6 +16,13 @@ function Video() {
           loop
           disable_player_controls
           enabled_player
+        /> */}
+        <video
+          src={'https://daghvm4jxqkzu.cloudfront.net/VP34+EIFS+Sep+24+DJI+0667+%5Bo1dcBgqcj2E%5D.mkv'}
+          autoPlay
+          muted
+          loop
+          // controls
         />
       </div>
     </div>
